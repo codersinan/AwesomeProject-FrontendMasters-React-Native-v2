f0a4371caf4016cdd796c2a3e8337a90222a6a9d
Sinan Caliskan <codersinan@gmail.com>
2020-05-19 00:32:33 +0300

    pull refresh example

--- screens/Home.js
+++ screens/Home.js
@@ -1,41 +1,10 @@
 import React, { useState, useCallback, useEffect } from 'react';
-
 import { FlatList, StyleSheet } from 'react-native';
-
 import PalettePreview from '../components/PalettePreview';
-
-const SOLARIZED = [
-    { colorName: 'Base03', hexCode: '#002b36' },
-    { colorName: 'Base02', hexCode: '#073642' },
-    { colorName: 'Base01', hexCode: '#586e75' },
-    { colorName: 'Green', hexCode: '#859900' },
-];
-
-const RAINBOW = [
-    { colorName: 'Red', hexCode: '#FF0000' },
-    { colorName: 'Orange', hexCode: '#FF7F00' },
-    { colorName: 'Yellow', hexCode: '#FFFF00' },
-    { colorName: 'Green', hexCode: '#00FF00' },
-    { colorName: 'Violet', hexCode: '#8B00FF' },
-];
-
-const FRONTEND_MASTERS = [
-    { colorName: 'Red', hexCode: '#c02d28' },
-    { colorName: 'Black', hexCode: '#3e3e3e' },
-    { colorName: 'Grey', hexCode: '#8a8a8a' },
-    { colorName: 'White', hexCode: '#ffffff' },
-    { colorName: 'Orange', hexCode: '#e66225' },
-];
-
-const COLOR_PALETTES = [
-    { paletteName: 'Solarized', colors: SOLARIZED },
-    { paletteName: 'Frontend Masters', colors: FRONTEND_MASTERS },
-    { paletteName: 'Rainbow', colors: RAINBOW },
-];
 
 const Home = ({ navigation }) => {
     const [colorPalettes, setColorPalettes] = useState([]);
-
+    const [isRefreshing, setIsRefreshing] = useState(false);
     const fetchColorPalettes = useCallback(async () => {
         const result = await fetch('https://color-palette-api.kadikraman.now.sh/palettes', {});
         if (result.ok) {
@@ -50,6 +19,15 @@ const Home = ({ navigation }) => {
     }, []);
 
 
+    const handleRefresh = useCallback(async () => {
+        setIsRefreshing(true);
+        await fetchColorPalettes();
+        setTimeout(() => {
+            setIsRefreshing(false);
+        }, 1000);
+
+    }, []);
+
     return (
         <FlatList
             style={styles.list}
@@ -63,6 +41,8 @@ const Home = ({ navigation }) => {
                     colorPalette={item}
                 />
             )}
+            refreshing={isRefreshing}
+            onRefresh={handleRefresh}
         />
     );
 };
