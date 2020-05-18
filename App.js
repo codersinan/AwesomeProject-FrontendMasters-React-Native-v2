1872e02cc9f0cecce11431ad45c65a48607a8840
Sinan Caliskan <codersinan@gmail.com>
2020-05-18 15:06:08 +0300

    add react navigation and stack

--- App.js
+++ App.js
@@ -1,48 +1,51 @@
+import 'react-native-gesture-handler';
+
 import React from 'react';
-import { Text, View, StyleSheet, SectionList, SafeAreaView } from 'react-native';
 
-const Food = props => {
-  return (
-    <View style={styles.food}>
-      <Text style={styles.text}>{props.name}</Text>
-    </View>
-  );
-}
+import { View, Text, SafeAreaView, StyleSheet, FlatList } from 'react-native';
+import ColorBox from './components/ColorBox';
 
-const FOODS = [
-  { title: 'Healthy', data: ['Apples', 'Broccoli'] },
-  { title: 'Not so Healthy', data: ['Cookies', 'Doritos', 'Eclairs'] },
+import { NavigationContainer } from "@react-navigation/native";
+const COLORS = [
+  { colorName: 'Base03', hexCode: '#002b36' },
+  { colorName: 'Base02', hexCode: '#073642' },
+  { colorName: 'Base01', hexCode: '#586e75' },
+  { colorName: 'Base00', hexCode: '#657b83' },
+  { colorName: 'Base0', hexCode: '#839496' },
+  { colorName: 'Base1', hexCode: '#93a1a1' },
+  { colorName: 'Base2', hexCode: '#eee8d5' },
+  { colorName: 'Base3', hexCode: '#fdf6e3' },
+  { colorName: 'Yellow', hexCode: '#b58900' },
+  { colorName: 'Orange', hexCode: '#cb4b16' },
+  { colorName: 'Red', hexCode: '#dc322f' },
+  { colorName: 'Magenta', hexCode: '#d33682' },
+  { colorName: 'Violet', hexCode: '#6c71c4' },
+  { colorName: 'Blue', hexCode: '#268bd2' },
+  { colorName: 'Cyan', hexCode: '#2aa198' },
+  { colorName: 'Green', hexCode: '#859900' },
 ];
 
 const App = () => {
   return (
-    <SafeAreaView>
-      <SectionList
-        sections={FOODS}
-        keyExtractor={item => item}
-        renderItem={data => <Food name={data.item} />}
-        renderSectionHeader={({ section }) => (
-          <Text style={styles.header}>{section.title}</Text>
-        )}
-      />
-    </SafeAreaView>
+    <NavigationContainer>
+      <SafeAreaView >
+        <FlatList
+          style={styles.container}
+          data={COLORS}
+          // keyExtractor={item => item.colorName}
+          renderItem={({ item }) => (
+            <ColorBox colorName={item.colorName} hexCode={item.hexCode} />
+          )}
+          ListHeaderComponent={<Text style={styles.text}>Solarized</Text>}
+        />
+      </SafeAreaView >
+    </NavigationContainer>
   );
 }
-
 const styles = StyleSheet.create({
-  food: {
-    justifyContent: 'center',
-    padding: 10,
-    backgroundColor: 'teal',
-    marginBottom: 10,
-  },
-  text: {
-    color: 'white',
-    fontWeight: 'bold'
-  },
-  header: {
-    fontSize: 18,
-    marginBottom: 5
+  container: {
+    paddingHorizontal: 10,
+    paddingTop: 40
   }
 });
 
